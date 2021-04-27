9790932f5464733b73371295fa14e7bdef01fe49
Damcho Lhendup <damcholhendup159@gmail.com>
2021-04-27 19:53:46 +0600

    TODO

---
+++ TODO_9/app/src/main/java/com/example/todo_9/Calculator.java
@@ -0,0 +1,23 @@
+package com.example.todo_9;
+
+public class Calculator {
+    public double Add (double value1, double value2 ){
+        return value1 + value2;
+
+    }
+
+    public double Sub (double value1, double value2 ){
+        return value1 - value2;
+
+    }
+
+    public double Mul (double value1, double value2 ){
+        return value1 * value2;
+
+    }
+
+    public double Div (double value1, double value2 ){
+        return value1 / value2;
+
+    }
+}
