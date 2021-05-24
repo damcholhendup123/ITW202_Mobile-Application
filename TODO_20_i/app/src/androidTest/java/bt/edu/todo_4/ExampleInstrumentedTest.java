0df667d7c23a642d19dd31897c25f81a070aa4ac
Damcho Lhendup <damcholhendup159@gmail.com>
2021-05-24 16:17:33 +0600

    TODO

---
+++ TODO_20_i/app/src/androidTest/java/bt/edu/todo_4/ExampleInstrumentedTest.java
@@ -0,0 +1,76 @@
+package bt.edu.todo_4;
+
+import android.content.Context;
+import android.view.View;
+
+import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.rule.ActivityTestRule;
+
+import org.hamcrest.Matcher;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule mActivityRule = new ActivityTestRule<>(MainActivity.class);
+
+
+    private static final String message = "in am damcho lhendup ";
+    private static final String reply = "Hi damcho!";
+
+
+    @Test
+    public void useAppContext() {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        assertEquals("bt.edu.todo_4", appContext.getPackageName());
+    }
+
+    @Test
+    public void activityLaunch(){
+        onView(withId(R.id.button_main)).perform(click());
+        onView(withId(R.id.text_header)).check(matches(isDisplayed()));
+        onView(withId(R.id.button_second)).perform(click());
+        onView(withId(R.id.text_header_reply)).check(matches(isDisplayed()));
+
+    }
+
+
+
+    @Test
+    public void textInputOutput(){
+        onView(withId(R.id.editText_main)).perform(
+                typeText("I am Damcho lhendup")
+        );
+        onView(withId(R.id.button_main)).perform(click());
+//        onView(withId(R.id.editText_second)).perform(
+//                typeText(reply),closeSoftKeyboard()
+//        );
+//        onView(withId(R.id.button_second)).perform(click());
+
+        onView(withId(R.id.text_message)).check(matches(withText("I am Damcho lhendup")));
+    }
+
+
+
+
+}
